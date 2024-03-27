92c7f1008d6d3b06f3a7ac2a379394aabc2fc56c
Michael lemito <delovoy.lemit@gmail.com>
2024-03-27 11:48:35 +0300

    Added a test range to visually reduce the number of cards + minor renaming

--- main/views.py
+++ main/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'main/index.html')
+    test_range = range(0, 12)
+    return render(request, 'main/index.html', {'test_range': test_range})
