bb3884f2b52d42faf6bff4d415c818b1053e618a
Max <lastochkin.maks05@mail.ru>
2024-04-17 19:30:47 +0300

    make_authorization

---
+++ users/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+from django.contrib.auth.forms import AuthenticationForm
+
+from users.models import User
+
+
+class UserLoginForm(AuthenticationForm):
+    class Meta:
+        model = User
+    
--- users/views.py
+++ users/views.py
@@ -1,8 +1,27 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.contrib import auth
+from django.urls import reverse
+
+from users.forms import UserLoginForm
 
 def login(request):
+    #Метод из документации обработки с кнопкой войти
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username=username, password=password)
+            if user:
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse('main:index'))
+    else:
+        form = UserLoginForm()
+
     context = {
-        'title': 'aboba'
+        'title': 'aboba',
+        'form': form
     }
     return render(request, 'users/login.html', context)
 
