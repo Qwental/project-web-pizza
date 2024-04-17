b45f0cf3711b93082a88600581b2e6a973ae9177
Max <lastochkin.maks05@mail.ru>
2024-04-18 00:20:14 +0300

    interim registration

--- users/views.py
+++ users/views.py
@@ -1,9 +1,9 @@
 from django.http import HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.contrib import auth
 from django.urls import reverse
 
-from users.forms import UserLoginForm
+from users.forms import UserLoginForm, UserRegistrationForm
 
 def login(request):
     #Метод из документации обработки с кнопкой войти
@@ -27,8 +27,20 @@ def login(request):
 
 
 def registration(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            user = form.instance
+            auth.login(request, user)
+            return HttpResponseRedirect(reverse('main:index'))
+    else:
+        form = UserRegistrationForm()
+
+
     context = {
-        'title': 'aboba'
+        'title': 'aboba',
+        'form': form
     }
     return render(request, 'users/registration.html', context)
 
@@ -41,4 +53,5 @@ def profile(request):
 
 
 def logout(request):
-    ...
+    auth.logout(request)
+    return redirect(reverse('main:index'))
