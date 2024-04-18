e66e88a1f5ba8366c31bd41dc25311ae669ca26a
emppress <rozhkovvanya.2005@yandex.ru>
2024-04-18 12:45:00 +0300

    minor fix

--- pizza/settings.py
+++ pizza/settings.py
@@ -144,6 +144,5 @@ INTERNAL_IPS = [
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-AUTH_USER_MODEL = 'users.User'
 
 
--- users/forms.py
+++ users/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.models import User
 
-from users.models import User
 
 
 class UserLoginForm(AuthenticationForm):
