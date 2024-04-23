647296542318719681992a1b352e12ad29dfa932
Michael lemito <delovoy.lemit@gmail.com>
2024-04-23 17:18:09 +0300

    Улучшил окошко выбора добавок
    Удалил ненужно, добавил окошек, немножко изменил python - теперь options отображается корректно

--- cart/views.py
+++ cart/views.py
@@ -5,11 +5,15 @@ from django.shortcuts import render
 from cart.models import Cart
 from main.models import Products
 
+# TODO: ВАНЯ ПОПРАВЬ __message, а то там всегда "Ошибка добавления товара в корзину" и не отображается успешный-попап
 def cart_add(request):
 
     data = json.loads(request.body)
+    print(data)
     product_id = data['productId']
-    #options = request.POST.get("options")
+    _options = data["options"]
+
+    _message = "Ошибка добавления товара в корзину"
 
     product = Products.objects.get(id=product_id)
     
@@ -22,9 +26,12 @@ def cart_add(request):
                     cart.quantity += 1
                     cart.save()
                     break
-            else: Cart.objects.create(user=request.user, product=product, quantity=1)
+            else:
+                Cart.objects.create(user=request.user, product=product, quantity=1) 
+                _message = "Товар добавлен в корзину"
         else:
-            Cart.objects.create(user=request.user, product=product, quantity=1, options={'a':'a'}, final_price=300)
+            Cart.objects.create(user=request.user, product=product, quantity=1, options=_options, final_price=300)
+            _message = "Товар добавлен в корзину"
 
     else:
         carts = Cart.objects.filter(
@@ -39,16 +46,18 @@ def cart_add(request):
             else: 
                 Cart.objects.create(
                 session_key=request.session.session_key, product=product, quantity=1)
+                _message = "Товар добавлен в корзину"
         else:
             Cart.objects.create(
                 session_key=request.session.session_key, product=product, quantity=1)
+            _message = "Товар добавлен в корзину"
     
     #user_cart = get_user_carts(request)
     #cart_items_html = render_to_string(
     #    "carts/includes/included_cart.html", {"carts": user_cart}, request=request)
 
     response_data = {
-        "message": "Товар добавлен в корзину",
+        "message": _message,
         #"cart_items_html": cart_items_html,
     }
 
