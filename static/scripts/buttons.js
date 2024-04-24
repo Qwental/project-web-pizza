e0de285926616573ee8e4f6c10a6b74f75332d0a
macbookirissskkk <irinka.timofeeva0505@gmail.com>
2024-04-24 22:35:24 +0300

    Add !!!

--- static/scripts/buttons.js
+++ static/scripts/buttons.js
@@ -38,3 +38,58 @@ title.addEventListener("click", (e) => {
     window.location.href = "/";
   }, 100);
 });
+
+
+* {
+    box-sizing: border-box;
+}
+
+* {
+    margin: 0;
+    padding: 0;
+}
+
+body {
+    font-family: 'Montserrat', sans-serif;
+}
+
+.block {
+    width: 600px;
+    height: 200px;
+    border-radius: 20px;
+    background-image: url(images/pizza1.jpg);
+    background-size: cover;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: left;
+}
+
+.block span {
+    font-size: 26px;
+    color: white;
+    margin: 10px 289px 0px 20px;
+    letter-spacing: 3px;
+}
+
+.btn {
+    width: 130px;
+    height: 45px;
+    border-radius: 12px;
+    filter: drop-shadow(0px 2px 2.5px rgba(33, 33, 33, 0.35));
+    background-color: #fd9824;
+    line-height: 45px;
+
+    color: white;
+    font-size: 18px;
+    font-weight: 700;
+    text-align: center;
+    display: inline-block;
+    text-decoration: none;
+    letter-spacing: 1px;
+    margin-left: 20px;
+    margin-bottom: 10px;
+}
+
+.btn:hover {
+    background-color: #388cfa;
+}
