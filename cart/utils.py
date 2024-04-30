ec7ce415b249981bfcf0479c04cb687feed33789
emppress <rozhkovvanya.2005@yandex.ru>
2024-04-30 19:59:55 +0300

    параметры товаров отображаются в корзине

--- cart/utils.py
+++ cart/utils.py
@@ -1,4 +1,6 @@
+import json
 from cart.models import Cart
+
 
 
 def get_user_carts(request):
@@ -8,3 +10,15 @@ def get_user_carts(request):
     if not request.session.session_key:
         request.session.create()
     return Cart.objects.filter(session_key=request.session.session_key).select_related('product')
+
+
+def get_product_options(cart: Cart):
+    options = cart.options
+    options_list = []
+    options_list.append(options['add'])
+    del options['add']
+    options_list.append([])
+    for item in options.items():
+        options_list[1].append(item)
+    return options_list
+
