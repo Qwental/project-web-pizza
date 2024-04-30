da5406ee689c7bba6d9b490952b274e6779139ed
Michael lemito <delovoy.lemit@gmail.com>
2024-04-30 18:42:35 +0300

    Динамичная корзина
    Теперть тут работают кнопки: добавить товар(+1), убавить(-1), удалить товар

---
+++ static/scripts/cart.js
@@ -0,0 +1,100 @@
+// Когда html документ готов (прорисован)
+$(document).ready(function () {
+    // Ловим собыитие клика по кнопке удалить товар из корзины
+    $(document).on("click", ".remove-from-cart", function (e) {
+        // Блокируем его базовое действие
+        e.preventDefault();
+
+        // Получаем id корзины из атрибута data-cart-id
+        var cart_id = $(this).data("cart-id");
+        // Из атрибута href берем ссылку на контроллер django
+        var remove_from_cart = $(this).attr("href");
+
+        // делаем post запрос через ajax не перезагружая страницу
+        $.ajax({
+
+            type: "POST",
+            url: remove_from_cart,
+            data: {
+                cart_id: cart_id,
+                csrfmiddlewaretoken: $("[name=csrfmiddlewaretoken]").val(),
+            },
+            success: function (data) {
+                // Меняем содержимое корзины на ответ от django (новый отрисованный фрагмент разметки корзины)
+                var cartItemsContainer = $("#cart-items-container");
+                cartItemsContainer.html(data.cart_items_html);
+
+            },
+
+            error: function (data) {
+                console.log("Ошибка при добавлении товара в корзину");
+            },
+        });
+    });
+
+
+
+
+    // Теперь + - количества товара 
+    // Обработчик события для уменьшения значения
+    $(document).on("click", ".decrement", function () {
+        // Берем ссылку на контроллер django из атрибута data-cart-change-url
+        var url = $(this).data("cart-change-url");
+        console.log(url);
+        // Берем id корзины из атрибута data-cart-id
+        var cartID = $(this).data("cart-id");
+        console.log(cartID);
+        // Ищем ближайшеий input с количеством 
+        var $input = $(this).closest('.input-group').find('.number');
+        // Берем значение количества товара
+        var currentValue = parseInt($input.val());
+        console.log(currentValue);
+        // Если количества больше одного, то только тогда делаем -1
+        if (currentValue > 1) {
+            $input.val(currentValue - 1);
+            // Запускаем функцию определенную ниже
+            // с аргументами (id карты, новое количество, количество уменьшилось или прибавилось, url)
+            updateCart(cartID, currentValue - 1, -1, url);
+        }
+    });
+
+    // Обработчик события для увеличения значения
+    $(document).on("click", ".increment", function () {
+        // Берем ссылку на контроллер django из атрибута data-cart-change-url
+        var url = $(this).data("cart-change-url");
+        // Берем id корзины из атрибута data-cart-id
+        var cartID = $(this).data("cart-id");
+        // Ищем ближайшеий input с количеством 
+        var $input = $(this).closest('.input-group').find('.number');
+        // Берем значение количества товара
+        var currentValue = parseInt($input.val());
+
+        $input.val(currentValue + 1);
+
+        // Запускаем функцию определенную ниже
+        // с аргументами (id карты, новое количество, количество уменьшилось или прибавилось, url)
+        updateCart(cartID, currentValue + 1, 1, url);
+    });
+
+    function updateCart(cartID, quantity, change, url) {
+        $.ajax({
+            type: "POST",
+            url: url,
+            data: {
+                cart_id: cartID,
+                quantity: quantity,
+                csrfmiddlewaretoken: $("[name=csrfmiddlewaretoken]").val(),
+            },
+
+            success: function (data) {
+                // Меняем содержимое корзины
+                var cartItemsContainer = $("#cart-items-container");
+                cartItemsContainer.html(data.cart_items_html);
+
+            },
+            error: function (data) {
+                console.log("Ошибка при добавлении товара в корзину: ", data);
+            },
+        });
+    }
+});
